da101d28ffbac0f8d4dec42a5987352ffbd912da
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-12 14:40:14 +0800

    test

--- server/server.py
+++ server/server.py
@@ -2,3 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/members")
+def members():
+    return{"members": ["1", "2", "3"]}
+
+if __name__ == "__main__":
+    app.run(debug=True)
